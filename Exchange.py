df5f72bac4b885c34753f67dbc63793a80d32385
priyacoding <136785066+priyacoding@users.noreply.github.com>
2023-10-24 15:46:12 +0530

    Create Exchange.py

---
+++ Exchange.py
@@ -0,0 +1,27 @@
+a=int(input("Enter the amount you have:"))
+b=a//500
+c=a%500
+d=c//200
+e=c%200
+f=e//100
+g=e%100
+h=g//50
+i=g%50
+j=i//20
+k=i%20
+l=k//10
+m=k%10
+n=m//5
+o=m%5
+p=o//2
+q=o%2
+r=q//1
+print('Total count for Five Hundred',b)
+print('Total count for Two Hundred',d)
+print('Total count for Hundred',f)
+print('Total count for Fifty',h)
+print('Total count for Twenty',j)
+print('Total count for Ten',l)
+print('Total count for Five',n)
+print('Total count for Two',p)
+print('Total count for One',r)
